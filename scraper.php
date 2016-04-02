2e5604fe63b8d14e7421ccb523e6ca3a271bdf1c
MengLuo <mluo.bupt@gmail.com>
2016-04-02 16:15:52 -0400

    updated php file to get parameter from html page

--- scraper.php
+++ scraper.php
@@ -1,8 +1,14 @@
 <?php
 
-echo
-file_get_contents("http://www.weather-forecast.com/locations/Waltham/forecasts/latest");
+$city = $_GET['city'];
 
+$city = str_replace(" ","",$city);
+
+$contents = file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
+
+preg_match('/3 Day Weather Forecast Summary:<\/b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">(.*?)</s', $contents, $matches);
+
+print_r($matches[1]);
 
 ?>
 
